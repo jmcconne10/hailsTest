a7c8cac30d8cc7744d1a4ac885a928bf1a025876
Joe McConnell <joe.mcconnell10@gmail.com>
2019-09-10 08:04:38 -0500

    commented out print statements and added comments

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,30 +1,33 @@
-import boto3
+import boto3 #this is what allows us to work with AWS
 import io
 from io import StringIO
-import mimetypes
+import mimetypes #this does something to the file that allows it to be viewed as a website
+import zipfile # this appears to let me unzip the zip file
 
-s3 = boto3.resource('s3')
-portfolio_bucket = s3.Bucket('jmcconnellportfolio.com')
-for obj in portfolio_bucket.objects.all():
-     print (obj.key)
+s3 = boto3.resource('s3') #opens s3 for us
+portfolio_bucket = s3.Bucket('jmcconnellportfolio.com') # sets destination folder
 
-#portfolio_bucket.download_file('index.html', '/tmp/index.html')
-build_bucket = s3.Bucket('hailstestbuild.jmcconnellportfolio.com')
-for obj in build_bucket.objects.all():
-     print (obj.key)
-
-#build_bucket.download_file('portfoliobuild.zip', '/tmp/portfoliobuild.zip')
+#prints out all of the items in destination folder
+#for obj in portfolio_bucket.objects.all():
+#     print (obj.key)
 
 
-portfolio_zip = io.BytesIO()
+build_bucket = s3.Bucket('hailstestbuild.jmcconnellportfolio.com') # set source folder
 
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+#prints out all of the items in the source folder
+#for obj in build_bucket.objects.all():
+#     print (obj.key)
 
-import zipfile
+portfolio_zip = io.BytesIO() # creates an object for the folder to go in
+
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip) # puts the zip file into object so it can be unzipped
+
+
+# unzips zip file
 with zipfile.ZipFile(portfolio_zip) as myzip:
      for nm in myzip.namelist():
-         print (nm)
-         obj=myzip.open(nm)
+         # print (nm) #prints files in the zip
+         obj=myzip.open(nm) # unzips file
          mime_type = mimetypes.guess_type(nm)[0]
-         portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+         portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]}) # copies files to destination
+         portfolio_bucket.Object(nm).Acl().put(ACL='public-read') # sets permission
