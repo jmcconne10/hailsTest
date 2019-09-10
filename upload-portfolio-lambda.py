2cb161d0baa34a8ca8f0c94c908e6e74d9355f44
Joe McConnell <joe.mcconnell10@gmail.com>
2019-09-10 07:05:29 -0500

    starting fresh

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,18 +1,28 @@
 import boto3
-from botocore.client import Config
-import StringIO
-import zipfile
+import io
+from io import StringIO
 
-s3 = boto3.resource('s3',config=Config(signature_version='s3v4'))
-
+s3 = boto3.resource('s3')
 portfolio_bucket = s3.Bucket('jmcconnellportfolio.com')
-build_bucket - s3.Bucket('hailstestbuild.jmcconnellportfolio.com')
+for obj in portfolio_bucket.objects.all():
+     print (obj.key)
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('jmcconnellportfolio.com', portfolio_zip)
+#portfolio_bucket.download_file('index.html', '/tmp/index.html')
+build_bucket = s3.Bucket('hailstestbuild.jmcconnellportfolio.com')
+for obj in build_bucket.objects.all():
+     print (obj.key)
 
+#build_bucket.download_file('portfoliobuild.zip', '/tmp/portfoliobuild.zip')
+
+
+portfolio_zip = io.BytesIO()
+
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+import zipfile
 with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm)
-        portfolio_zip.Object(nm).Acl().put(ACL='public-read')
+     for nm in myzip.namelist():
+         print (nm)
+         obj=myzip.open(nm)
+         portfolio_bucket.upload_fileobj(obj,nm)
+         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
