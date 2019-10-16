e2e193a30ed950ceff7ef1de8d3f3ae15244317d
Joe McConnell <joe.mcconnell10@gmail.com>
2019-10-15 20:40:29 -0500

    adding cognito config

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_crOU5AIAK', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'h6jbftebjp10i7sp66ra9bn3a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
